0374aa4ddbcd997b9e1550243a734df5c7c3469e
Borre Pol Meeuwisse <borre@wearebit.com>
2022-09-23 14:34:20 +0200

    Added functionality for retrieving database items; created a notebook for testing the class; added type-hinting for most methods; etc.

--- knowledge_base.py
+++ knowledge_base.py
@@ -12,8 +12,17 @@ class Request():
 
     headers = {
         'Authorization': 'Bearer ' + integration_token,
-        'Content-Type': 'application/json',
         'Notion-Version': '2022-06-28'
+    }
+
+    post_headers = {
+        **headers,
+        'Content-Type': 'application/json',
+    }
+
+    get_headers = {
+        **headers,
+        'accept': 'application/json',
     }
 
     data = {
@@ -21,13 +30,16 @@ class Request():
         'properties': {}
     }
 
-    def resetData(self):
+    def __init__(self) -> None:
+        self.terms, self.definitions = self.parseDatabaseContents()
+
+    def resetData(self) -> None:
         self.data = {
             'parent': {'database_id': self.database_id},
             'properties': {}
         }
 
-    def mapResponse(self, code: int, message: str = 'short'):
+    def mapResponse(self, code: int, message: str = 'short') -> str:
         if message not in ['short', 'long']:
             print('Specify if you either want a "long" or "short" response message.')
 
@@ -36,7 +48,7 @@ class Request():
         else:
             return responses[code][1]
 
-    def addProperty(self, name: str, type: str, val):
+    def addProperty(self, name: str, type: str, val) -> None:
         if type == 'title':
             self.data['properties'][name] = {
                 'type': type,
@@ -53,7 +65,7 @@ class Request():
                 type: val
             }
 
-    def viewData(self):
+    def viewData(self) -> None:
         print(self.data)
 
     def createDatabaseEntry(self):
@@ -62,77 +74,67 @@ class Request():
         self.resetData()
         return requests.request('POST', self.url, headers=self.headers, data=data)
 
+    def retrieveDatabaseContents(self, database_id: str = ''):
+        if database_id == '':
+            database_id = self.database_id
+
+        url = f'https://api.notion.com/v1/databases/{database_id}/query'
+        payload = {'page_size': 100}
+
+        return requests.post(url, json=payload, headers=self.get_headers)
+
+    def parseDatabaseContents(self, database_id: str = ''):
+        terms = []
+        definitions = []
+        response = self.retrieveDatabaseContents(database_id)
+
+        for row in response.json()['results']:
+            properties = row['properties']
+            terms.append(properties['Term']['title'][0]['plain_text'])
+            definitions.append(properties['Definition']['rich_text'][0]['plain_text'])
+
+        return terms, definitions
+
     def retrievePageProperties(self, page_id: str):
         url = f'{self.url}/{page_id}'
-
-        headers = {
-            'Authorization': 'Bearer ' + self.integration_token,
-            "accept": "application/json",
-            "Notion-Version": "2022-06-28"
-        }
-
-        return requests.get(url, headers=headers)
+        return requests.get(url, headers=self.get_headers)
 
     def retrievePageContent(self, page_id: str):
         url = f'https://api.notion.com/v1/blocks/{page_id}/children'
+        return requests.get(url, headers=self.get_headers)
 
-        headers = {
-            'Authorization': 'Bearer ' + self.integration_token,
-            "accept": "application/json",
-            "Notion-Version": "2022-06-28"
-        }
-
-        return requests.get(url, headers=headers)
-
-    def getCallouts(self, page_id: str):
+    def retrieveCallouts(self, page_id: str) -> list:
         callouts = []
         response = self.retrievePageContent(page_id)
         for block in response.json()['results']:
             if block['type'] == 'callout':
                 callouts.append(block['callout']['rich_text'][0]['plain_text'])
 
-        self.callouts = callouts
         return callouts
 
-    def processDefinitions(self):
-        self.terms = []
-        self.definitions = []
-        for d in self.callouts:
-            term, definition = d.split(':')
+    def processDefinitions(self, page_id: str) -> None:
+        callouts = self.retrieveCallouts(page_id)
+        for c in callouts:
+            term, definition = c.split(':')
+
+            if term in self.terms and definition in self.definitions:
+                continue
+
             self.terms.append(term)
             self.definitions.append(definition.strip())
-            # print(f'Term: {term}, Definition: {definition}')
 
-    def insertDefinitions(self):
+    def insertDefinitions(self) -> list:
         # into database that is
+        responses = []
 
         for t, d in zip(self.terms, self.definitions):
-            print(type(t))
-            print(type(d))
+            # Check if the term-definition pair already exists, if not, create a new entry.
+            # You don't have to check if it already exists if you can guarantee that the local lists in this object are always up
+            # to date.
             self.addProperty('Term', 'title', t)
             self.addProperty('Definition', 'rich_text', d)
 
-            self.viewData()
-
             r = self.createDatabaseEntry()
-            print(self.mapResponse(r.status_code, 'long'))
+            responses.append(r)
 
-
-r = Request()
-# r.addProperty('Term', 'title', 'Testing')
-# r.addProperty('Definition', 'rich_text', 'tester')
-# r.viewData()
-# response = r.createDatabaseEntry()
-
-# print(r.mapResponse(response.status_code))
-# print(response.text)
-
-# page = r.retrievePageContent('2ef5d88e5cf94281aaadd7dc43eac6cf')
-# print(r.mapResponse(page.status_code))
-
-# with open('./pagecontent.json', 'w', encoding='utf8') as f:
-#     json.dump(page.text, f, ensure_ascii=False)
-
-print(r.getCallouts('2ef5d88e5cf94281aaadd7dc43eac6cf'))
-r.processDefinitions()
-r.insertDefinitions()
+        return responses
